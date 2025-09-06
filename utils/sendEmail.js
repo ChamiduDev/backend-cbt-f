41343c7ae2f5838ae3bfdefa33c1303e57b1c3df
Chamindu Kavishka <chamindu.kavi2002@gmail.com>
2025-09-06 20:39:47 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,15 +2,20 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
+    host: 'smtp.mailersend.net',
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
+      user: 'MS_mOIKDB@test-zkq340exqnkgd796.mlsender.net',
+      pass: 'mssp.LzHkEIH.3zxk54vkx9xljy6v.VtceXle',
     },
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 
   const mailOptions = {
-    from: process.env.EMAIL_FROM,
+    from: 'MS_mOIKDB@test-zkq340exqnkgd796.mlsender.net',
     to: options.email,
     subject: options.subject,
     html: options.message,
