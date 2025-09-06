8c625c0aa7f7327b7b78d351705983378a3f1873
Chamindu Kavishka <chamindu.kavi2002@gmail.com>
2025-09-06 21:55:26 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,7 +4,7 @@ const resend = new Resend('re_eZ9krXjU_D8XWQ7v657cn6EMfh8K8oZ8h');
 
 const sendEmail = async (options) => {
   await resend.emails.send({
-    from: 'Ceylon Black Taxi <noreply@ceylonblacktaxi.com>',
+    from: 'Ceylon Black Taxi <onboarding@resend.dev>',
     to: [options.email],
     subject: options.subject,
     html: options.message,
